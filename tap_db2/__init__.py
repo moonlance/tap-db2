29e0e490f0efccade0a4bbd88f765be7a2cde534
mjsqu <mj2.squ@gmail.com>
2022-08-22 11:46:39 +1200

    Changed handling for floats from DB2
    Changed handling for floats from DB2, which are at risk from losing precision in transfer and being truncated on targets
    
    Config option is use_singer_decimal

--- tap_db2/__init__.py
+++ tap_db2/__init__.py
@@ -163,7 +163,7 @@ def schema_for_column(c,config):
 
     elif data_type in FLOAT_TYPE_EXPONENT:
         if use_singer_decimal:
-            result.type = ["null","number"]
+            result.type = ["null","string"]
             result.format = "singer.decimal"
         else:
             result.type = ["null", "number"]
